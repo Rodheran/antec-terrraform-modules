93a79501ea47c04459229675752b98692df075df
Rodheran <30419717+Rodheran@users.noreply.github.com>
2020-03-25 00:03:15 -0300

    Update main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -1,9 +1,13 @@
+# Define el proveedor de nube
 provider "aws" {
+  #Establece una region 
   region = "us-east-2"
 }
-resource "aws_security_group" "ssh_conection" {
+# Indica la definicion de un security group, y le asigna un nombre para poder ser llamdo en el main.tf
+resource "aws_security_group" "anctec_instance_centos" {
+  #Nombre que será asignado al sg en aws
   name = var.sg_name
-  dynamic "ingress"{
+  dynamic "ingress"{#reglas de ingreso al sg que serán leidas desde el archivo de variables prod.tfvars
      for_each = var.ingress_rules
      content { 
         from_port   = ingress.value.from_port
@@ -14,29 +18,39 @@ resource "aws_security_group" "ssh_conection" {
      }
 
  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  dynamic "egress"{#reglas de Egreso al sg que serán leidas desde el archivo de variables prod.tfvars
+    for_each = var.egress_rules
+     content { 
+        from_port   = egress.value.from_port
+        to_port     = egress.value.to_port
+        protocol    = egress.value.protocol
+        cidr_blocks = egress.value.cidr_blocks #aws_vpc.main.cidr_block
+
+     }
+
+ }
 
   tags = {
     Name = "allow_tls"
   }
 }
-resource "aws_key_pair" "deployer" {
-  key_name   = "anctec-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFIe3pkqT0aJjxdc3pV694snRsDsjv7cLA1shWeFrPEQinEL1OfOwDoHuVwLOUpMiosuLF2pqAK39iSO24GV393wewHjfMCfOtAkTAHTO68Vpy8yEAja4PF35A1IqeLbEnRgBJQBUJ5I5Tta2zoXLQnt6RzsmmCfevgD6SDEIGKlWRnMqr8pXPyflvj/PbHI1vqqOT2mjHepYpjsFtw5d9bS0UEzbZKhLWMSl+QkTUaxtwTTrAS/fs1urE4+437/jP/hmfruaVTcEbX/IMtdbLfPZheCPD3Ieyrg65ZKm9jb0Pwxhe1LD/wHZYSV4fFfvQLYGfhMqPdFF+Qn4MtSKT adolf@DESKTOP-U6EP53Q"
-}
 
-resource "aws_instance" "platzi-instance" {
-  #ami = "ami-076db09ea35d146b6"
-  ami = var.ami_id
-  instance_type = var.instance_type
-  tags= var.tags 
-  security_groups = ["${aws_security_group.ssh_conection.name}"]
-  
+
+resource "aws_instance" "centos-instance" {# declara un  recurso instacia aws, y le asigna un nombre para acceso local en el codigo fuente
+  ami = var.ami_id # Asigna una ami que pasada por variable y que debe existir en Aws
+  instance_type = var.instance_type # Typo de instancia, para la capa gratuita de aws, utilzar t2.micro
+  key_name = var.key_pair_name  # define la llave que será utilizada para acceder a la instancia y que fue creada previamente en la consola aws
+  tags = var.tags # Buscando definicion 
+  security_groups = ["${aws_security_group.anctec_instance_centos.name}"] # Asigna a la instancia el SG definido al comienzo el archivo
+  provisioner "remote-exec"{ # hace una coneccion remota a la instancia una vez que se encuentra corriendo, falta afinar coneccion 
+    connection{
+      type =  "ssh"
+      user = "centos"
+      #private_key = file("~/.ssh/id_rsa")
+      host = self.public_ip
+    }
+    inline = ["echo hello "]
+  } 
   #{
    # Name = "practica1"
     #Environment = "Dev"
